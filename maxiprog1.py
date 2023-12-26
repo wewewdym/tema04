59ac0361a180b2caaf53749aede9071efc02240e
MacBook-Pro-Kristina <kristina_macbook@icloud.com>
2023-12-26 09:33:38 +0300

    maxiprog1.py changes

--- maxiprog1.py
+++ maxiprog1.py
@@ -1,15 +1,11 @@
-def find_maximum_value(input_list):
-if not input_list: # проверяем, что список не пуст
-return None
-
-max_value = input_list[0] # принимаем первое значение как начальное максимальное значение
-
-for num in input_list:
-if num > max_value: # если текущее число больше максимального значения
-max_value = num # обновляем максимальное значение
-
 return max_value
 
 input_list = [3, 8, 1, 6, 2, 7, 4, 5] # пример входного списка
-print("Максимальное значение в списке:", find_maximum_value(input_list))
+n=int(input())
+input_list =[]
 
+for i in range (n):
+num=int(input())
+input_list.append(num)
+
+print("Максимальное значение в списке:", find_maximum_value(input_list))
